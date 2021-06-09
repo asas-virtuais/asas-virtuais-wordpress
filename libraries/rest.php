b0b0514201178efbe69c1c4f060906b0c64c0c5b
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2021-06-09 19:30:26 -0300

    + API functions

---
+++ libraries/rest.php
@@ -0,0 +1,11 @@
+<?php
+namespace AsasVirtuaisWordpress;
+
+function register_rest_route( string $namespace, callable $route, array $args = [], $override = false ) {
+	$args['callback'] = safe_callback( $args['callback'] );
+	return \register_rest_route( $namespace, $route, $args, $override );
+}
+
+function ajax_action( string $action, callable $callback, $priority = 10, $accepted = 1 ) {
+	return add_safe_action( "wp_ajax_$action", $callback, $priority, $accepted );
+}
