a6a549adc738f1c5b3a84c6bfdb3019f00e9aa9d
Ícaro C. Capobianco <icaro.capobianco@gmail.com>
2021-06-09 20:01:55 -0300

    loading library

--- loader.php
+++ loader.php
@@ -2,22 +2,25 @@
 
 return function ( $autoloader ) {
 	$this_version = '1.1.0';
-	add_filter( 'asas.version', function ( $version ) use ( $this_version ) {
+	\add_filter( 'asas.version', function ( $version ) use ( $this_version ) {
 		if ( ! $version || version_compare( $this_version, $version, '>' ) ) {
 			return $this_version;
 		}
 		return $version;
 	} );
-	add_action( 'plugins_loaded', function() use ( $this_version, $autoloader ) {
+	\add_action( 'plugins_loaded', function() use ( $this_version, $autoloader ) {
 		$version = apply_filters( 'asas.version', null );
 		if ( $version !== $this_version )
 			return;
 		if ( did_action( 'asas.load' ) > 0 )
 			return;
-		$autoloader->addPsr4( 'AsasVirtuaisWordpress', [
-			__DIR__ . '/libraries/',
-			__DIR__ . '/includes/',
+		$autoloader->addPsr4( 'AsasVirtuaisWordpress\\', [
+			plugin_dir_path( __FILE__ ) . 'libraries',
+			plugin_dir_path( __FILE__ ) . 'includes',
 		] );
+		foreach( glob( plugin_dir_path( __FILE__ ) . 'libraries/*.php' ) as $file ) {
+			include $file;
+		}
 		do_action( 'asas.load', $this_version );
 	} );
 };
